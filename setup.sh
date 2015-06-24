91420684828ece2f8db8eac1b0db3a7899a62f06
zaPray <zapray.zhou@gmail.com>
2015-06-23 20:31:16 -0400

    add area seq fectching script in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,18 @@ mkdir data/comm
 
 # fetch meta data
 curl -L -o data/GC.meta https://www.dropbox.com/s/4bzem6yu6dweny8/GC.meta?dl=0
+
+# fetch move data
 curl -L -o data/move/park-movement-Fri.bin https://www.dropbox.com/s/6z3zkwp6rs01vy7/park-movement-Fri.bin?dl=0
 curl -L -o data/move/park-movement-Sat.bin https://www.dropbox.com/s/sy12i36s811iie0/park-movement-Sat.bin?dl=0
 curl -L -o data/move/park-movement-Sun.bin https://www.dropbox.com/s/w5omplnlddw8tba/park-movement-Sun.bin?dl=0
 
+# fetch comm data
 curl -L -o data/comm/comm-data-Fri.bin https://www.dropbox.com/s/rc80kpil5vypqax/comm-data-Fri.bin?dl=0
 curl -L -o data/comm/comm-data-Sat.bin https://www.dropbox.com/s/7q2vcgd917pezjv/comm-data-Sat.bin?dl=0
 curl -L -o data/comm/comm-data-Sun.bin https://www.dropbox.com/s/ea5w7cgc3x3sy70/comm-data-Sun.bin?dl=0
+
+# fetch area seq data
+curl -L -o https://www.dropbox.com/s/6wqhjpl7jmj1s4g/area-sequence-Fri.bin?dl=0
+curl -L -o https://www.dropbox.com/s/ta6qj53f6d79shn/area-sequence-Sat.bin?dl=0
+curl -L -o https://www.dropbox.com/s/alktdu56oetv970/area-sequence-Sun.bin?dl=0
